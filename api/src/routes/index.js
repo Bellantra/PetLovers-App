4810a24d34e48c38e874b2844d450802aa5fb8a7
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-05-25 14:59:50 -0400

    reorder routes/index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const userRoutes = require('./userRoutes')
 const adoptRoutes = require('./adoptRoutes')
-
-router.use('/users', userRoutes)
 router.use('/adopt', adoptRoutes)
+
+const userRoutes = require('./userRoutes')
+router.use('/users', userRoutes)
 
 const shelterRoutes = require('./shelterRoutes')
 router.use('/shelters', shelterRoutes)
