5ae52eddcbc6c1fd4f4eabbfd7e3275df7c3a0df
Alicia Pak <aliciappak@gmail.com>
2022-05-27 09:04:23 -0300

    carrousel added

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,12 +1,14 @@
 /* import React from 'react'; */
 import AboutUs from '../components/Home/AboutUs';
 import Hero from '../components/Home/Hero'
+import Carrousel from '../components/Home/Carrousel';
 
 const Home = () => {
     return(
         <>
             <Hero />
             <AboutUs/>
+            <Carrousel/>
         </>
     )
 }
