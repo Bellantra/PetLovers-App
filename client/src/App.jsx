d7f86bf25b90722625178b52fa6bd8902f274020
Bellantra <yamilapaez2@hotmail.com>
2022-05-27 02:14:18 +0200

    working shelter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
                     path="/underConstruction"
                     element={<UnderConstruction />}
                 />
-                <Route path="/shelter/:id" element={<Shelter />} />
+                <Route path="/shelter" element={<Shelter />} />
             </Routes>
         </BrowserRouter>
     )
