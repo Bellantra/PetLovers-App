fa598a086bc75e5d5665e5e5a42e6651cd07f909
Bellantra <yamilapaez2@hotmail.com>
2022-05-27 14:03:48 +0200

    shelter created

--- client/src/components/Home/Shelters.jsx
+++ client/src/components/Home/Shelters.jsx
@@ -1,4 +1,10 @@
-import { Grid } from '@mui/material'
+import {
+    Card,
+    CardActionArea,
+    CardContent,
+    CardMedia,
+    Grid,
+} from '@mui/material'
 import Typography from '@mui/material/node/Typography'
 import { Box } from '@mui/system'
 import { createTheme } from '@mui/material/styles'
@@ -9,10 +15,33 @@ import { useEffect } from 'react'
 const theme = createTheme({
     palette: {
         text: {
-            primary: '#FFFFFF',
+            primary: '#E9D5CA',
         },
     },
 })
+
+const shelters1 = [
+    {
+        name: 'Patitas de Glew',
+        logo: 'https://res.cloudinary.com/petlovers1/image/upload/v1653649543/Shelters/Logos/logo4.png',
+    },
+    {
+        name: 'Arca de Noe',
+        logo: 'https://res.cloudinary.com/petlovers1/image/upload/v1653649499/Shelters/Logos/logo3.png',
+    },
+    {
+        name: 'Los Bandidos de Lomas',
+        logo: 'https://res.cloudinary.com/petlovers1/image/upload/v1653649455/Shelters/Logos/logo2.png',
+    },
+    {
+        name: 'Refugio Las renatas',
+        logo: 'https://res.cloudinary.com/petlovers1/image/upload/v1653649434/Shelters/Logos/logo1.png',
+    },
+    {
+        name: 'Los gatitos de Nora',
+        logo: 'https://res.cloudinary.com/petlovers1/image/upload/v1653649594/Shelters/Logos/logo6.png',
+    },
+]
 
 const Shelters = () => {
     const dispatch = useDispatch()
@@ -25,7 +54,7 @@ const Shelters = () => {
     console.log(shelters)
 
     return (
-        <Grid bgcolor={'#660000'}>
+        <Grid bgcolor={'#293462'}>
             <Box paddingY={5}>
                 <Typography
                     theme={theme}
@@ -33,10 +62,64 @@ const Shelters = () => {
                     fontWeight={800}
                     color={'text.primary'}
                     align="center"
+                    marginY={10}
                 >
-                    Nuestros Refugios
+                    Our Shelters
                 </Typography>
-                {shelters ? <Grid></Grid> : <div>Loading</div>}
+                <Grid
+                    container
+                    spacing={2}
+                    gap={3}
+                    justifyContent={'space-around'}
+                >
+                    {shelters1 ? (
+                        shelters1.map((shelter, index) => (
+                            // <Grid
+                            //     container
+                            //     key={index}
+                            //     bgcolor={'#E9D5CA'}
+                            //     maxWidth={250}
+                            //     maxHeight={250}
+                            // >
+                            //     <Grid item minWidth={150}>
+                            //         <img alt="Example Alt" src={shelter.logo} />
+                            //     </Grid>
+
+                            //     <Grid item>
+                            //         <Typography variant={'h6'}>
+                            //             {shelter.name}
+                            //         </Typography>
+                            //     </Grid>
+                            // </Grid>
+                            <Card
+                                sx={{ maxWidth: 345 }}
+                                key={index}
+                                align="center"
+                                item
+                            >
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        maxWidth={250}
+                                        image={shelter.logo}
+                                        alt="green iguana"
+                                    />
+                                    <CardContent>
+                                        <Typography
+                                            gutterBottom
+                                            variant="h5"
+                                            component="div"
+                                        >
+                                            {shelter.name}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        ))
+                    ) : (
+                        <div>Loading</div>
+                    )}
+                </Grid>
             </Box>
         </Grid>
     )
