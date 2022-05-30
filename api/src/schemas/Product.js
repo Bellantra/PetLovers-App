276eb581aa9c38eae9eefdb8c268cdab137b340f
Bellantra <yamilapaez2@hotmail.com>
2022-05-30 19:50:15 +0200

    Product Schema created

---
+++ api/src/schemas/Product.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    img: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    stock: {
+        type: Number,
+        required,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    shelter: {
+        type: Schema.Types.ObjectId,
+        ref: 'Shelter',
+        default: null, //Porque null gabriel???
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Paused', 'Deleted'],
+        default: 'Active',
+    },
+})
+
+module.exports = mongoose.model('Product', productSchema)
