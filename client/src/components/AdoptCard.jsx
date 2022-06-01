5f0a3bb176bf61a4c35da31fe0e4b743df436980
Eliana <enicotra83@gmail.com>
2022-05-31 21:30:33 -0300

    .

--- client/src/components/AdoptCard.jsx
+++ client/src/components/AdoptCard.jsx
@@ -4,15 +4,23 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
+import { useState } from 'react'
+import styled from 'styled-components'
+import Modal from './Modal'
+import UnderConstruction from './UnderConstruction'
 
 // eslint-disable-next-line react/prop-types
 export default function AdoptCard({ pet }) {
     // eslint-disable-next-line react/prop-types
     const { image, nickname, age } = pet
+    const [estadoModal1, setEstadoModal1] = useState(false)
 
     return (
         <div>
-            <Card sx={{ maxWidth: 250 }}>
+            <Card
+                sx={{ maxWidth: 250 }}
+                onClick={() => setEstadoModal1(!estadoModal1)}
+            >
                 <CardMedia
                     component="img"
                     alt="Imagen de un animal"
@@ -32,6 +40,56 @@ export default function AdoptCard({ pet }) {
                     <Button size="small">Adopt</Button>
                 </CardActions>
             </Card>
+
+            <Modal
+                estado={estadoModal1}
+                setEstado={setEstadoModal1}
+                titulo=""
+                mostrarHeader={true}
+                mostrarOverlay={true}
+                /* lo puedo sacar, queda center por defecto */
+                posicionModal={'center'}
+                /* lo puedo sacar, queda 20px por defecto */
+                padding={'20px'}
+            >
+                <Contenido>
+                    <CardMedia
+                        component="img"
+                        alt="Imagen de un animal"
+                        height="140"
+                        image={image[0]}
+                    />
+
+                    <Typography gutterBottom variant="h5" component="div">
+                        {nickname}
+                    </Typography>
+
+                    <UnderConstruction></UnderConstruction>
+                </Contenido>
+            </Modal>
         </div>
     )
 }
+
+const Contenido = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+        font-size: 42px;
+        font-weight: 700;
+        margin-bottom: 10px;
+    }
+
+    p {
+        font-size: 18px;
+        margin-bottom: 20px;
+    }
+
+    img {
+        width: 100%;
+        vertical-align: top;
+        border-radius: 3px;
+    }
+`
