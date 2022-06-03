4f60da2d0d943e84d349a716aaaab3badbd3a639
Bellantra <yamilapaez2@hotmail.com>
2022-06-03 23:01:26 +0200

    new carousel component

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,23 +1,26 @@
 import { Carousel } from 'react-carousel-minimal'
 
+// Pueden personalizando pasando las props con otros valores. Al final pisa las props
+// que trae por defecto, asi que vuelvan a pasar la prop con el valor a cambiar
+
 const CarouselCustom = (props) => {
-    // eslint-disable-next-line react/prop-types
     const data = props.images.map((elem) => ({ image: elem }))
     return (
         <>
             <Carousel
-                {...props}
                 data={data}
                 automatic={true}
                 time={5000}
                 radius="5px"
                 dots={true}
-                slideImageFit="cover"
-                slideBackgroundColor="darkgrey"
+                slideImageFit="contain"
+                thumbnails={false}
+                slideBackgroundColor="#EDEDED"
                 style={{
                     textAlign: 'center',
                     maxHeight: '100vh',
                 }}
+                {...props}
             ></Carousel>
         </>
     )
