08e2b320ae24777ddbd7fc612c3eeac13c596dc4
Yamila Paez <yamilapaez2@hotmail.com>
2024-08-13 20:29:31 +0200

    fix password user

--- api/src/schemas/User.js
+++ api/src/schemas/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-    nickname: {
+    userName: {
         type: String,
         unique: true,
     },
